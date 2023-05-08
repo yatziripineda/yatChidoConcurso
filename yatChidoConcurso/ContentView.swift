d0931960295473394dc15c2fa192d77befb3ac95
yatziripineda <131062655+yatziripineda@users.noreply.github.com>
2023-05-08 13:57:02 -0600

    Add files via upload

---
+++ yatChidoConcurso/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  yatChidoConcurso
+//
+//  Created by CEDAM05 on 08/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ yatChidoConcurso/yatChidoConcursoApp.swift
@@ -0,0 +1,17 @@
+//
+//  yatChidoConcursoApp.swift
+//  yatChidoConcurso
+//
+//  Created by CEDAM05 on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct yatChidoConcursoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
